b3d4d6ac46707a9d09a307b6d492473c68460863
Krzysztof Soporek <k.soporek@gmail.com>
2019-06-15 20:20:51 +0200

    Add: Custom validation attribute

---
+++ src/FlowerShop/FlowerShop/Models/CustomValidation/NoHarshWordsAllowed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace FlowerShop.Models.CustomValidation
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class NoHarshWordsAllowed : ValidationAttribute
+    {
+        private List<string> harshWords = new List<string>()
+        {
+            "ugly", "redundant", "eyesore"
+        };
+
+        public override bool IsValid(object value)
+        {
+            string userFeedback = value as string;
+
+            if (string.IsNullOrEmpty(userFeedback))
+                return true;
+
+            var userWords = userFeedback.Split(' ');
+            bool harshSentence = userWords.Any(uw => harshWords.Contains(uw));
+
+            return !harshSentence;
+        }
+    }
+}
--- src/FlowerShop/FlowerShop/Models/Feedback.cs
+++ src/FlowerShop/FlowerShop/Models/Feedback.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.ComponentModel.DataAnnotations;
+using FlowerShop.Models.CustomValidation;
 
 namespace FlowerShop.Models
 {
@@ -17,8 +18,9 @@ namespace FlowerShop.Models
         [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
            ErrorMessage = "Incorrect email format.")]
         public string Email { get; set; }
-
+        
         [Required(ErrorMessage = "Please, add message")]
+        [NoHarshWordsAllowed(ErrorMessage = "Please be nice.")]
         [StringLength(5000, ErrorMessage = "Max message lenght: 5000")]
         public string Message { get; set; }
 
